39c61ea2774a0ddd83838b77516518535d87ac06
Pulsaronline <beslite@gmail.com>
2021-04-11 11:56:44 +0300

    added another web browser support

--- src/test/java/tests/StudentRegistrationFormTests.java
+++ src/test/java/tests/StudentRegistrationFormTests.java
@@ -1,17 +1,14 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import pages.StudentRegistrationFormPage;
+        import com.codeborne.selenide.Configuration;
+        import io.qameta.allure.selenide.AllureSelenide;
+        import org.junit.jupiter.api.*;
+        import org.openqa.selenium.remote.DesiredCapabilities;
+        import pages.StudentRegistrationFormPage;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
-import static helpers.AttachmentHelper.*;
+        import static com.codeborne.selenide.Selenide.closeWebDriver;
+        import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+        import static helpers.AttachmentHelper.*;
 
 public class StudentRegistrationFormTests {
     StudentRegistrationFormPage formPage;
@@ -24,35 +21,35 @@ public class StudentRegistrationFormTests {
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
-        Configuration.remote = System.getProperty("remote");
+//      gradle clean test -Dweb.browser = safari
+        Configuration.browser = System.getProperty("web.browser", "chrome");
+        //Configuration.remote = System.getProperty("remote");
         //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
-    }
+        //gradle clean test -Dremote.web.driver="https://user1:1234@selenoid.autotests.cloud/wd/hub/"
+        String remoteWebDriver = System.getProperty("remote.web.driver");
+        if(remoteWebDriver !="")
+            Configuration.remote = remoteWebDriver;
 
-   /* @BeforeEach
-    void beforeEach(){
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        Configuration.browserCapabilities = capabilities;
-        Configuration.remote = System.getProperty("remote_driver");
-    }*/
+    }
 
     @AfterEach
     public void afterEach() {
         attachScreenshot("Last screenshot");
         attachPageSource();
         attachAsText("Browser console error logs", getConsoleLogs());
-//        if (System.getProperty("video_storage") != null)
-        attachVideo();
+//gradle clean test -Dremote.web.driver="https://user1:1234@selenoid.autotests.cloud/wd/hub/"
+// -Dvideo.storage="https://selenoid.autotests.cloud/video/"
+        if(System.getProperty("video.storage") !="")
+            attachVideo();
         closeWebDriver();
     }
 
     @Test
     @DisplayName("Positive test")
     void okTest() {
-       formPage = new StudentRegistrationFormPage();
-       formPage.fillThePage("");
-          }
+        formPage = new StudentRegistrationFormPage();
+        formPage.fillThePage("");
+    }
 
     @Test
     @DisplayName("Negative test")
