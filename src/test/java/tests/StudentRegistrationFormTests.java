53705e036ed284d984b8399b2f546ea01ba17f76
Pulsaronline <beslite@gmail.com>
2021-04-11 15:32:08 +0300

    added secret login/password config files

--- src/test/java/tests/StudentRegistrationFormTests.java
+++ src/test/java/tests/StudentRegistrationFormTests.java
@@ -1,18 +1,23 @@
 package tests;
 
         import com.codeborne.selenide.Configuration;
-        import io.qameta.allure.selenide.AllureSelenide;
-        import org.junit.jupiter.api.*;
-        import org.openqa.selenium.remote.DesiredCapabilities;
-        import pages.StudentRegistrationFormPage;
+import config.DriverConfig;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import pages.StudentRegistrationFormPage;
 
-        import static com.codeborne.selenide.Selenide.closeWebDriver;
-        import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
-        import static helpers.AttachmentHelper.*;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.AttachmentHelper.*;
 
 public class StudentRegistrationFormTests {
     StudentRegistrationFormPage formPage;
-
+    static DriverConfig driverConfig = ConfigFactory.create(DriverConfig.class);
     @BeforeAll
     public static void beforeAll() {
         Configuration.browserSize = "1024x768";
@@ -26,10 +31,18 @@ public class StudentRegistrationFormTests {
         //Configuration.remote = System.getProperty("remote");
         //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
         //gradle clean test -Dremote.web.driver="https://user1:1234@selenoid.autotests.cloud/wd/hub/"
+        //gradle clean test -Dremote.web.driver="https://%s:%s@selenoid.autotests.cloud/wd/hub/"
         String remoteWebDriver = System.getProperty("remote.web.driver");
-        if(remoteWebDriver !="")
-            Configuration.remote = remoteWebDriver;
+        if(remoteWebDriver !=null) {
+            String user = driverConfig.RemoteWebUser();
+            String password = driverConfig.RemoteWebPassword();
+            Configuration.remote = String.format(remoteWebDriver, user, password);
 
+            System.out.println(user);
+            System.out.println(password);
+            System.out.println(System.getProperty(remoteWebDriver));
+            System.out.println(String.format(remoteWebDriver, user, password));
+        }
     }
 
     @AfterEach
